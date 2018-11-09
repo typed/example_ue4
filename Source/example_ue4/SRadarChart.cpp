04adf2635fd59af955c8e9a0e79fd2391bd3d41f
typed <gxj21720@126.com>
2018-11-09 11:34:27 +0800

    1

--- Source/example_ue4/SRadarChart.cpp
+++ Source/example_ue4/SRadarChart.cpp
@@ -38,13 +38,28 @@ void SRadarChart::SetBrush(const FSlateBrush& __Brush)
 
 void SRadarChart::SetProgress(int32 i, float prg)
 {
-    Progress.FindOrAdd(i) = FMath::Clamp(prg, MinProgress, 1.f);
+    Progress.FindOrAdd(i) = FMath::Clamp(prg, 0.f, 1.f);
 }
 
 void SRadarChart::ResetProgress()
 {
     Progress.Empty();
 }
+
+#define ComputeSlateVertex(ant, col) \
+    float tmp; \
+    float Theta = i * WedgeAngle; \
+    auto pf = Progress.Find(i); \
+    if (pf) \
+        tmp = radius * (*pf); \
+    else \
+        tmp = radius; \
+    tmp += ant; \
+    vec.X = PtCenter.X + tmp * FMath::Cos(Theta); \
+    vec.Y = PtCenter.Y - tmp * FMath::Sin(Theta); \
+    TexCoords.X = 0.5f + (vec.X - PtCenter.X) / diameter; \
+    TexCoords.Y = 0.5f + (vec.Y - PtCenter.Y) / diameter; \
+    v = FSlateVertex::Make<ESlateVertexRounding::Disabled>(transform, vec, TexCoords, col);
 
 int32 SRadarChart::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
 {
@@ -68,20 +83,9 @@ int32 SRadarChart::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeom
     FVector2D TexCoords(0.5f, 0.5f);
     v = FSlateVertex::Make<ESlateVertexRounding::Disabled>(transform, PtCenter, TexCoords, FColor::White);
     av.Add(v);
-    float Theta, tmp;
     FVector2D vec;
     for (int32 i = 0; i < SideCount; i++) {
-        Theta = i * WedgeAngle;
-        auto pf = Progress.Find(i);
-        if (pf)
-            tmp = radius * (*pf);
-        else
-            tmp = radius;
-        vec.X = PtCenter.X + tmp * FMath::Cos(Theta);
-        vec.Y = PtCenter.Y - tmp * FMath::Sin(Theta);
-        TexCoords.X = 0.5f + (vec.X - PtCenter.X) / diameter;
-        TexCoords.Y = 0.5f + (vec.Y - PtCenter.Y) / diameter;
-        v = FSlateVertex::Make<ESlateVertexRounding::Disabled>(transform, vec, TexCoords, FColor::White);
+        ComputeSlateVertex(0.f, FColor::White)
         av.Add(v);
     }
     for (int32 i = 1; i <= SideCount; i++) {
@@ -95,23 +99,19 @@ int32 SRadarChart::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeom
         FColor BdCol = FColor::White;
         BdCol.A = 0.f;
         for (int32 i = 0; i < SideCount; i++) {
-            Theta = i * WedgeAngle;
-            auto pf = Progress.Find(i);
-            if (pf)
-                tmp = radius * (*pf);
-            else
-                tmp = radius;
-            tmp = tmp + Antialias;
-            vec.X = PtCenter.X + tmp * FMath::Cos(Theta);
-            vec.Y = PtCenter.Y - tmp * FMath::Sin(Theta);
-            TexCoords.X = 0.5f + (vec.X - PtCenter.X) / diameter;
-            TexCoords.Y = 0.5f + (vec.Y - PtCenter.Y) / diameter;
-            v = FSlateVertex::Make<ESlateVertexRounding::Disabled>(transform, vec, TexCoords, BdCol);
+            ComputeSlateVertex(Antialias, BdCol)
             av.Add(v);
         }
+        int32 next, next_border;
         for (int32 i = 1; i <= SideCount; i++) {
-            int32 next = i == SideCount ? 1 : i + 1;
-            int32 next_border = i == SideCount ? SideCount + 1 : SideCount + i + 1;
+            if (i == SideCount) {
+                next = 1;
+                next_border = SideCount + 1;
+            }
+            else {
+                next = i + 1;
+                next_border = SideCount + i + 1;
+            }
             ai.Add(i);
             ai.Add(SideCount + i);
             ai.Add(next);
