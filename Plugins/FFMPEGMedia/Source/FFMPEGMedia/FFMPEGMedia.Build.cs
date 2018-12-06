b3d2d34f709219014ffd66f4d2ffd0f6620477b1
typed <gxj21720@126.com>
2018-12-06 21:44:23 +0800

    1

--- Plugins/FFMPEGMedia/Source/FFMPEGMedia/FFMPEGMedia.Build.cs
+++ Plugins/FFMPEGMedia/Source/FFMPEGMedia/FFMPEGMedia.Build.cs
@@ -23,15 +23,25 @@ public class FFMPEGMedia : ModuleRules
 
 	public bool LoadFFmpeg(ReadOnlyTargetRules Target)
 	{
-		bool isLibrarySupported = false;
+        bool isLibrarySupported = false;
+        
+		string PlatformString = "win64";
+        
+        if (Target.Platform == UnrealTargetPlatform.Win64) {
+            PlatformString = "win64";
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Win32) {
+            PlatformString = "win32";
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Mac) {
+            PlatformString = "mac";
+        }
+        
+        string LibrariesPath = Path.Combine(ThirdPartyPath, "ffmpeg", PlatformString, "lib");
 
 		if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
 		{
 			isLibrarySupported = true;
-
-			string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "win32";
-			string LibrariesPath = Path.Combine(Path.Combine(Path.Combine(ThirdPartyPath, "ffmpeg", "lib"), "vs"), PlatformString);
-
 
 			System.Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
 
@@ -50,24 +60,23 @@ public class FFMPEGMedia : ModuleRules
         else if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
 			isLibrarySupported = true;
-			string LibrariesPath = Path.Combine(Path.Combine(ThirdPartyPath, "ffmpeg", "lib"), "osx");
-
-			System.Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+			
+            System.Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
             
-            string[] libs = {"libavcodec.58.dylib","libavdevice.58.dylib", "libavfilter.7.dylib", "libavformat.58.dylib", "libavutil.56.dylib", "libswresample.3.dylib", "libswscale.5.dylib", "libpostproc.55.dylib"};
-            foreach (string lib in libs)
-            {
-                PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, lib));
-                PublicDelayLoadDLLs.Add(Path.Combine(LibrariesPath, lib));
-	            RuntimeDependencies.Add(Path.Combine(LibrariesPath, lib), StagedFileType.NonUFS);
-            }
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libavcodec.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libavdevice.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libavfilter.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libavformat.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libavutil.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libswresample.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libswscale.a"));
 
 		}
 
 		if (isLibrarySupported)
 		{
 			// Include path
-			PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "ffmpeg", "include"));
+			PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "ffmpeg", PlatformString, "include"));
 		}
 
 
