6fd51dad5daf8464aa678153280beaa50ca2f8b8
typed <gxj21720@126.com>
2019-04-24 23:41:22 +0800

    1

--- Source/example_ue4/UReuseListC.cpp
+++ Source/example_ue4/UReuseListC.cpp
@@ -599,13 +599,10 @@ void UReuseListC::OnWidgetRebuilt()
     SyncProp();
     auto wld = GetWorld();
     if (wld && !wld->IsGameWorld()) {
-        GetWorld()->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateLambda([this]() {
-            if (ScrollBoxList->IsValidLowLevelFast() &&
-                CanvasPanelBg->IsValidLowLevelFast() &&
-                SizeBoxBg->IsValidLowLevelFast() &&
-                CanvasPanelList->IsValidLowLevelFast())
-            {
-                Reload(PreviewCount);
+        TWeakObjectPtr<UReuseListC> self = this;
+        wld->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateLambda([=]() {
+            if (self.IsValid()) {
+                self->Reload(PreviewCount);
             }
         }));
     }
