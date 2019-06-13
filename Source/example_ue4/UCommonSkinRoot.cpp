127394450e088b36e4ae0516e94dc32f3d6c3d68
typed <gxj21720@126.com>
2019-06-13 20:50:17 +0800

    皮肤管理

---
+++ Source/example_ue4/UCommonSkinRoot.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UCommonSkinRoot.h"
+#include "Runtime/UMG/Public/Components/CanvasPanel.h"
+
+DEFINE_LOG_CATEGORY(LogUCommonSkinRoot);
+
+UCommonSkinRoot::UCommonSkinRoot(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+    , CanvasPanelRoot(nullptr)
+{
+}
+
+bool UCommonSkinRoot::Initialize()
+{
+    if (!Super::Initialize())
+        return false;
+    InitWidgetPtr();
+    return true;
+}
+
+void UCommonSkinRoot::NativeConstruct()
+{
+    Super::NativeConstruct();
+    InitWidgetPtr();
+}
+
+void UCommonSkinRoot::InitWidgetPtr()
+{
+    CanvasPanelRoot = Cast<UCanvasPanel>(GetWidgetFromName(FName(TEXT("CanvasPanelRoot"))));
+    ensure(CanvasPanelRoot.IsValid());
+}
+
+void UCommonSkinRoot::OnWidgetRebuilt()
+{
+    Super::OnWidgetRebuilt();
+    InitWidgetPtr();
+}
+
+UUserWidget* UCommonSkinRoot::LoadSkin(FString __WidgetPath)
+{
+    TWeakObjectPtr<UUserWidget> widget = GetAndCreateWidgetFromPool(__WidgetPath);
+    if (!widget.IsValid())
+        return nullptr;
+    TWeakObjectPtr<UUserWidget> widget_src = Cast<UUserWidget>(CanvasPanelRoot->GetChildAt(0));
+    if (widget_src.IsValid() && widget_src == widget)
+        return widget_src.Get();
+    CanvasPanelRoot->ClearChildren();
+    CanvasPanelRoot->AddChild(widget.Get());
+    return widget.Get();
+}
+
+TWeakObjectPtr<UUserWidget> UCommonSkinRoot::GetAndCreateWidgetFromPool(const FString& __WidgetPath)
+{
+    TWeakObjectPtr<UUserWidget>* ptr_widget = SkinPool.Find(__WidgetPath);
+    if (ptr_widget && (*ptr_widget).IsValid())
+        return *ptr_widget;
+    TSubclassOf<UUserWidget> uclass = ::LoadClass<UUserWidget>(nullptr, *__WidgetPath);
+    if (uclass == nullptr)
+        return nullptr;
+    UUserWidget* widget = CreateWidget<UUserWidget>(GetWorld(), uclass);
+    if (widget == nullptr) {
+        UE_LOG(LogUCommonSkinRoot, Error, TEXT("UCommonSkinRoot CreateUserWidget Widget == nullptr"));
+        return nullptr;
+    }
+    UE_LOG(LogUCommonSkinRoot, Log, TEXT("UCommonSkinRoot CreateUserWidget Widget:%x Name: %s"), widget, *(widget->GetName()));
+    SkinPool.Add(__WidgetPath, widget);
+    return widget;
+}
---
+++ Source/example_ue4/UCommonSkinRoot.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+/**************************************************************************
+Author: levingong
+Date: 2019-6-13
+Description: »»Æ¤»º´æ
+**************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UCommonSkinRoot.generated.h"
+
+/**
+*
+*/
+
+class UCanvasPanel;
+
+UCLASS()
+class EXAMPLE_UE4_API UCommonSkinRoot : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+
+    UCommonSkinRoot(const FObjectInitializer& ObjectInitializer);
+
+    UFUNCTION(BlueprintCallable)
+    UUserWidget* LoadSkin(FString __WidgetPath);
+
+protected:
+
+    bool Initialize();
+
+    void NativeConstruct();
+
+    void OnWidgetRebuilt();
+
+    void InitWidgetPtr();
+
+    TWeakObjectPtr<UUserWidget> UCommonSkinRoot::GetAndCreateWidgetFromPool(const FString& __WidgetPath);
+
+    TWeakObjectPtr<UCanvasPanel> CanvasPanelRoot;
+
+    TMap<FString, TWeakObjectPtr<UUserWidget> > SkinPool;
+};
+
+DECLARE_LOG_CATEGORY_EXTERN(LogUCommonSkinRoot, Verbose, All);
