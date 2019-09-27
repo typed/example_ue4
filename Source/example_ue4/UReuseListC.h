7814dbaf6a626018c904dc3691baa82c72b3ec76
typed <gxj21720@126.com>
2019-09-27 16:59:49 +0800

    1

--- Source/example_ue4/UReuseListC.h
+++ Source/example_ue4/UReuseListC.h
@@ -107,6 +107,12 @@ public:
     int32 GetPaddingY() const { return PaddingY; }
 
     UFUNCTION(BlueprintCallable)
+    int32 GetItemWidthAndPaddingX() const { return ItemWidth + PaddingX; }
+
+    UFUNCTION(BlueprintCallable)
+    int32 GetItemHeightAndPaddingY() const { return ItemHeight + PaddingY; }
+
+    UFUNCTION(BlueprintCallable)
     void JumpByIdx(int32 __Idx) { JumpByIdxStyle(__Idx, EReuseListJumpStyle::Middle); }
 
     UFUNCTION(BlueprintCallable)
